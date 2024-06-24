8794d76f2daab6682fadf21685bd2976dadb6f33
kruzznitsch <rougemeister1@gmail.com>
2024-06-24 11:44:13 +0000

    Created the clock object

---
+++ script.js
@@ -0,0 +1,41 @@
+const currentTime = new Date();
+const hours = currentTime.getHours();
+const minutes = currentTime.getMinutes();
+const seconds = currentTime.getSeconds();
+
+console.log(`Current Time: ${hours}:${minutes}:${seconds}`);
+
+
+const clock = {
+    hours: hours,
+    minutes: minutes,
+    seconds: seconds
+}
+
+
+console.log(clock.hours)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
