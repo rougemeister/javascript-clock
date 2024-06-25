59f808ac20509522faf60670093abcc4be4c607a
kruzznitsch <rougemeister1@gmail.com>
2024-06-25 08:18:34 +0000

    Added updated clock object

--- script.js
+++ script.js
@@ -1,39 +1,16 @@
-const currentTime = new Date();
-const hours = currentTime.getHours();
-const minutes = currentTime.getMinutes();
-const seconds = currentTime.getSeconds();
+const digitalClock = document.querySelector('.digital-clock');
+const clockhours = document.querySelector('.hour');
+const clockminutes = document.querySelector('.minutes');
+const clockseconds = document.querySelector('.seconds');
+const timeFormat = document.querySelector('.time-format');
 
-console.log(`Current Time: ${hours}:${minutes}:${seconds}`);
 
-
-const clock = {
-    hours: hours,
-    minutes: minutes,
-    seconds: seconds
+function Clock() {
+    this.hours = 0;
+    this.minutes = 0;
+    this.seconds = 0;
+    console.log(this.hours, this.minutes, this.seconds);
 }
-
-
-console.log(clock.hours)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 
