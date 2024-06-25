c15effe7e305ceaf113239d61ff4d75cbdae02af
kruzznitsch <rougemeister1@gmail.com>
2024-06-25 08:19:24 +0000

    Added clock object methods

--- script.js
+++ script.js
@@ -14,5 +14,60 @@ function Clock() {
 
 
 
+Object.defineProperties(Clock.prototype, {
+    getFormattedTime: {
+        value: function() {
+            const formattedHours = this.hours.toString().padStart(2, '0');
+            const formattedMinutes = this.minutes.toString().padStart(2, '0');
+            const formattedSeconds = this.seconds.toString().padStart(2, '0');
+            console.log(formattedHours);
+            return `${formattedHours}:${formattedMinutes}:${formattedSeconds}`;
+        },
+        writeable: true,
+        configuration: true,
+        enumerable: false
+    },
+    get12HourTime: {
+        value: function() {
+            let timePeriod = "AM";
+            if(this.hours >= 12){
+                timePeriod = "PM"
+                if(this.hours > 12){
+                    this.hours -= 12;
+                }
+            } else if(this.hours === 0){
+                this.hours = 12;
+            }
+            return timePeriod;
+        },
+        writeable: true,
+        configuration: true,
+        enumerable: false
+    },
+    update: {
+        value: function(hours, minutes, seconds) {
+            this.hours = hours;
+            this.minutes = minutes;
+            this.seconds = seconds;
+            this.display();
+        },
+        writeable: true,
+        configuration: true,
+        enumerable: false
+    },
+    display: {
+        value: function() {
+            clockhours.textContent = this.hours.toString().padStart(2, '0');
+            clockminutes.textContent = this.minutes.toString().padStart(2, '0');
+            clockseconds.textContent = this.seconds.toString().padStart(2, '0');
+            timeFormat.textContent = this.get12HourTime();
+        }
+    }
+})
+
+
+
+
+
 
 
